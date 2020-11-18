4fc0f0a04789767f755a0e39a57d89ea90571436
Misha Kushka <kushka.misha@gmail.com>
2020-11-18 14:20:07 +0200

    Remove redundancy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const { encode58 } = require('./base58');
 const { getBase58CheckAddress } = require('./crypto')
 
 const hex = '411a6ac17c82ad141ebc524a9ffc94965848f35279' // TCNtTa1rveKkovHR2ebABu4K66U6ocUCZX
-const bytes = [...Buffer.from(hex, 'hex')];
-// console.log(bytes);
-console.log(getBase58CheckAddress(bytes));
-console.log(getBase58CheckAddress(bytes) === 'TCNtTa1rveKkovHR2ebABu4K66U6ocUCZX')
+console.log(getBase58CheckAddress(hex) === 'TCNtTa1rveKkovHR2ebABu4K66U6ocUCZX')
+console.log(getBase58CheckAddress('412eb90f8356345c903d9f85e58d1b8177890adfb6') === 'TEEFn7rQqx4Xc3GL1Bx27A155xAj7w5W7a')
+console.log(getBase58CheckAddress('4149a5f0cda413ab723fff9baf956329ecfe5d1a23') === 'TGgd7pXdZALo9GyT4pmF2tT6JRf7ETWVcL')
