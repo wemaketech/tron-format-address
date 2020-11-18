c1db1c3f83e4379d2beedaf49893c0fa764990f6
Misha Kushka <kushka.misha@gmail.com>
2020-11-18 16:04:27 +0200

    Move to TypeScript

---
+++ lib/crypto.ts
@@ -0,0 +1,27 @@
+import crypto from 'crypto'
+import { encode58, decode58 } from './base58'
+
+const sha256 = (msg: string) => crypto.createHash('sha256').update(Buffer.from(msg, 'hex')).digest('hex')
+
+export const fromHex = (hex: string): string => {
+  const doubleSha256 = sha256(sha256(hex))
+  const checkSum = doubleSha256.substr(0, 8)
+  const address = Buffer.from(hex + checkSum, 'hex')
+
+  return encode58(address)
+}
+
+export const toHex = (base58Sting: string): string => {
+  if (base58Sting.length <= 4) throw new Error('Invalid address provided')
+
+  let address = Buffer.from(decode58(base58Sting)).toString('hex')
+
+  const checkSum = address.substr(-8)
+  address = address.substr(0, address.length - 8)
+
+  const checkSum1 = sha256(sha256(address)).substr(0, 8)
+
+  if (`${checkSum}` === `${checkSum1}`) return address
+
+  throw new Error('Invalid address provided')
+}
---
+++ test/crypto.test.ts
@@ -0,0 +1,42 @@
+import { expect } from 'chai'
+import { toHex, fromHex } from '../lib/crypto'
+
+describe('From Base58 to Hexadecimal', () => {
+  it('#1', () => {
+    const expectedHex = '411a6ac17c82ad141ebc524a9ffc94965848f35279'
+    const hex = toHex('TCNtTa1rveKkovHR2ebABu4K66U6ocUCZX')
+    expect(hex).to.equal(expectedHex)
+  })
+
+  it('#2', () => {
+    const expectedHex = '412eb90f8356345c903d9f85e58d1b8177890adfb6'
+    const hex = toHex('TEEFn7rQqx4Xc3GL1Bx27A155xAj7w5W7a')
+    expect(hex).to.equal(expectedHex)
+  })
+
+  it('#3', () => {
+    const expectedHex = '4149a5f0cda413ab723fff9baf956329ecfe5d1a23'
+    const hex = toHex('TGgd7pXdZALo9GyT4pmF2tT6JRf7ETWVcL')
+    expect(hex).to.equal(expectedHex)
+  })
+})
+
+describe('From Hexadecimal to Base58', () => {
+  it('#1', () => {
+    const expectedBase58 = 'TCNtTa1rveKkovHR2ebABu4K66U6ocUCZX'
+    const base58 = fromHex('411a6ac17c82ad141ebc524a9ffc94965848f35279')
+    expect(base58).to.equal(expectedBase58)
+  })
+
+  it('#2', () => {
+    const expectedBase58 = 'TEEFn7rQqx4Xc3GL1Bx27A155xAj7w5W7a'
+    const base58 = fromHex('412eb90f8356345c903d9f85e58d1b8177890adfb6')
+    expect(base58).to.equal(expectedBase58)
+  })
+
+  it('#3', () => {
+    const expectedBase58 = 'TGgd7pXdZALo9GyT4pmF2tT6JRf7ETWVcL'
+    const base58 = fromHex('4149a5f0cda413ab723fff9baf956329ecfe5d1a23')
+    expect(base58).to.equal(expectedBase58)
+  })
+})
